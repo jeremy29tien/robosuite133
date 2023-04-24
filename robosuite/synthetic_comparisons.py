ac40a35354bea6054e777b32c6bb8aba07eda759
Jeremy Tien <jeremy29tien@gmail.com>
2023-04-23 23:03:32 -0700

    modify synth comparisons to include gt reward and to include commands

--- robosuite/synthetic_comparisons.py
+++ robosuite/synthetic_comparisons.py
@@ -2,6 +2,8 @@ import numpy as np
 from robosuite.environments.manipulation.lift_features import speed, height, distance_to_bottle, distance_to_cube
 
 
+greater_gtreward_adjs = ["better", "more successfully"]
+less_gtreward_adjs = ["worse", "not as well"]
 greater_speed_adjs = ["faster", "quicker", "swifter", "at a higher speed"]
 less_speed_adjs = ["slower", "more moderate", "more sluggish", "at a lower speed"]
 greater_height_adjs = ["higher", "taller", "at a greater height"]
@@ -12,15 +14,29 @@ less_distance_adjs = ["closer", "nearer", "more nearby"]
 
 # This function takes in two trajectories in the form of LISTS of (observation, action) pairs.
 # Features:
+# GT reward (mean)
 # Speed (mean)
 # Height (mean)
 # Distance to cube (final)
 # Distance to bottle (min)
-def generate_synthetic_comparisons(traj1, traj2, feature_name):
+def generate_synthetic_comparisons_statements(traj1, traj2, traj1_gtrewards, traj2_gtrewards, feature_name):
     horizon = len(traj1)
     traj1_feature_values = None
     traj2_feature_values = None
-    if feature_name == "speed":
+    if feature_name == "gt_reward":
+        traj1_feature_values = [traj1_gtrewards[t] for t in range(horizon)]
+        traj2_feature_values = [traj2_gtrewards[t] for t in range(horizon)]
+
+        if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):  # Here, we take the MEAN gt_reward
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in greater_gtreward_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in less_gtreward_adjs]
+            return ordinary_comps + flipped_comps
+        else:
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in less_gtreward_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in greater_gtreward_adjs]
+            return ordinary_comps + flipped_comps
+
+    elif feature_name == "speed":
         traj1_feature_values = [speed(traj1[t]) for t in range(horizon)]
         traj2_feature_values = [speed(traj2[t]) for t in range(horizon)]
 
@@ -72,4 +88,66 @@ def generate_synthetic_comparisons(traj1, traj2, feature_name):
             ordinary_comps = ["The first trajectory is " + w + " to the cube than the second trajectory." for w in less_distance_adjs]
             flipped_comps = ["The second trajectory is " + w + " from the cube than the first trajectory." for w in greater_distance_adjs]
             return ordinary_comps + flipped_comps
+
+
+# NOTE: For this function, we produce commands that would change traj1 to traj2.
+def generate_synthetic_comparisons_commands(traj1, traj2, traj1_gtrewards, traj2_gtrewards, feature_name):
+    horizon = len(traj1)
+    traj1_feature_values = None
+    traj2_feature_values = None
+    if feature_name == "gt_reward":
+        traj1_feature_values = [traj1_gtrewards[t] for t in range(horizon)]
+        traj2_feature_values = [traj2_gtrewards[t] for t in range(horizon)]
+
+        if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):  # Here, we take the MEAN gt_reward
+            commands = ["Lift the cube " + w + "." for w in greater_gtreward_adjs]
+            return commands
+        else:
+            commands = ["Lift the cube " + w + "." for w in less_gtreward_adjs]
+            return commands
+
+    elif feature_name == "speed":
+        traj1_feature_values = [speed(traj1[t]) for t in range(horizon)]
+        traj2_feature_values = [speed(traj2[t]) for t in range(horizon)]
+
+        if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):  # Here, we take the MEAN speed
+            commands = ["Move " + w + "." for w in greater_speed_adjs]
+            return commands
+        else:
+            commands = ["Move " + w + "." for w in less_speed_adjs]
+            return commands
+
+    elif feature_name == "height":
+        traj1_feature_values = [height(traj1[t]) for t in range(horizon)]
+        traj2_feature_values = [height(traj2[t]) for t in range(horizon)]
+
+        if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):  # Here, we take the MEAN height
+            commands = ["Move " + w + "." for w in greater_height_adjs]
+            return commands
+        else:
+            commands = ["Move " + w + "." for w in less_height_adjs]
+            return commands
+
+    elif feature_name == "distance_to_bottle":
+        traj1_feature_values = [distance_to_bottle(traj1[t]) for t in range(horizon)]
+        traj2_feature_values = [distance_to_bottle(traj2[t]) for t in range(horizon)]
+
+        if np.min(traj1_feature_values) > np.min(traj2_feature_values):  # Here, we take the MINIMUM distance
+            commands = ["Move " + w + " from the bottle." for w in greater_distance_adjs]
+            return commands
+        else:
+            # Move further from the bottle.
+            commands = ["Move " + w + " to the bottle." for w in less_distance_adjs]
+            return commands
+
+    elif feature_name == "distance_to_cube":
+        traj1_feature_values = [distance_to_cube(traj1[t]) for t in range(horizon)]
+        traj2_feature_values = [distance_to_cube(traj2[t]) for t in range(horizon)]
+
+        if traj1_feature_values[-1] > traj2_feature_values[-1]:  # Here, we take the FINAL distance
+            commands = ["Move " + w + " from the cube." for w in greater_distance_adjs]
+            return commands
+        else:
+            commands = ["Move " + w + " to the cube." for w in less_distance_adjs]
+            return commands
 
